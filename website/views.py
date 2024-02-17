6a529b522c197da8a7d633e105b2da742f4769ec
AlexandraGardner <77805798+AlexandraGardner@users.noreply.github.com>
2024-02-17 13:16:52 -0500

    Navigation and web pages completely set up

--- website/views.py
+++ website/views.py
@@ -14,6 +14,18 @@ def explore():
 def deforestationInMD():
     return render_template("views/deforestationInMD.html")
 
+@views.route("/statistics")
+def statistics():
+    return render_template("views/statistics.html")
+
+@views.route("/featuredContent")
+def featuredContent():
+    return render_template("views/featuredContent.html")
+
+@views.route("/howToHelp")
+def howToHelp():
+    return render_template("views/howToHelp.html")
+
 @views.route("/contact")
 def contact():
     return render_template("views/contact.html")
