d7b8661406cf168184174db9e743df15df75d7f0
AlexandraGardner <77805798+AlexandraGardner@users.noreply.github.com>
2024-02-16 21:27:18 -0500

    Basic web app setup

---
+++ website/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from website import views
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "ourgreenspace2024"
+
+    app.register_blueprint(views.views)
+
+    return app
